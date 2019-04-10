5d2ac28af3b43344d75159246e4e57cf5b14776a
Michael Moody <dcmoods@gmail.com>
2019-04-10 18:51:10 -0400

    Update est.sh

--- est.sh
+++ est.sh
@@ -3,6 +3,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
-hadoop com.sun.tools.javac.Main Estimator.java Pair.java
-jar cf est.jar Estimator*.class Pair*.class
+hadoop com.sun.tools.javac.Main Project3_846/src/Estimator.java Project3_846/src/Pair.java
+jar cf est.jar Project3_846/src/Estimator*.class Project3_846/src/Pair*.class
 
